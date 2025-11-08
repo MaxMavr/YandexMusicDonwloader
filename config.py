6180846a6b56189ab0828b54fa82f1a06da0561b
MaxMavr <maxmavr2005@gmail.com>
2025-11-08 18:02:00 +0300

    release 1.4.3
    Refactor: DOWNLOAD_PATH transfer to env

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+from os import getenv
 from pathlib import Path
+from dotenv import find_dotenv, load_dotenv
 
 VERSION = "1.4.2"
 
-DOWNLOAD_PATH = Path("L:/video photo music/music/Скаченное")
+load_dotenv(find_dotenv())
+DOWNLOAD_PATH = Path(getenv('DOWNLOAD_PATH'))
 TEMP_PATH = DOWNLOAD_PATH / "temp"
 STATE_FILE = TEMP_PATH / "state.json"
