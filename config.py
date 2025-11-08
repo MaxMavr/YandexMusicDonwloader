82aa3ce0ed8d20f13f9ed5571dc82bfc604828bd
MaxMavr <maxmavr2005@gmail.com>
2025-11-08 17:39:52 +0300

    release 1.4.2
    Add: files to simplified and beautiful run

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-VERSION = "1.4.1"
+VERSION = "1.4.2"
 
 DOWNLOAD_PATH = Path("L:/video photo music/music/Скаченное")
 TEMP_PATH = DOWNLOAD_PATH / "temp"
---
+++ main.bat
@@ -0,0 +1,3 @@
+@echo off
+mode con: cols=172 lines=61
+start cmd /k "mode con: cols=172 lines=62 && python main.py"
